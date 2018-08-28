d7691342ac4b3020d3e46a4f6e7c05c53c21841e
paul_os\posullivan <paul_os2002@yahoo.co.uk>
2018-08-28 20:36:56 +0100

    Trying out some sample code - Adding an ngOnChanges hook

--- src/app/test-ground/test-ground-child/test-ground-child.component.ts
+++ src/app/test-ground/test-ground-child/test-ground-child.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-test-ground-child',
   templateUrl: './test-ground-child.component.html',
   styleUrls: ['./test-ground-child.component.css']
 })
-export class TestGroundChildComponent implements OnInit {
+export class TestGroundChildComponent implements OnInit, OnChanges {
 
   @Input("initialValue")
   counter: number;
@@ -16,6 +16,27 @@ export class TestGroundChildComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /*
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges() method");
+    let change: SimpleChange = changes.counter;
+    console.log(change.currentValue);
+    console.log(change.previousValue);
+  }
+  */
+
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+
+    // for-in is a JS iteration expression (where prop is the property name and not the value)
+    for(let prop in changes) {
+      console.log(`property name = ${prop}`);
+      // Access the name using the bracket notation as opposed to the dot notation
+      console.log(`current value = ${changes[prop].currentValue}`);
+      console.log(`previous value = ${changes[prop].previousValue}`);
+    }
+
   }
 
   increment() {
